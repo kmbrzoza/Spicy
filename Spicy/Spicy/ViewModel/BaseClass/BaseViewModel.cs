3b70d420eaa22a38353239a8de2615749602ddf9
Brzoza Krzysztof <kmbrzoza@gmail.com>
2021-06-10 11:18:25 +0200

    initial commit

---
+++ Spicy/Spicy/ViewModel/BaseClass/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTC.ViewModel.BaseClass
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged(params string[] properties)
+        {
+            if (PropertyChanged != null)
+                foreach (var property in properties)
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
