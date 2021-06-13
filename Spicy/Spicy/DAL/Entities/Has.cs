62a4aa7130c07433b72611b01b14cd4c89b484f4
kami011 <kamila.g2067@gmail.com>
2021-06-13 23:47:17 +0200

    Relations added.

---
+++ Spicy/Spicy/DAL/Entities/Has.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Spicy.DAL.Entities
+{
+    class Has
+    {
+        #region Properties
+        public uint? Id_s { get; set; }
+        public uint? Id_p { get; set; }
+        #endregion
+
+        #region Constructors
+        public Has(MySqlDataReader reader)
+        {
+            Id_s = uint.Parse(reader["id_s"].ToString());
+            Id_p = uint.Parse(reader["id_p"].ToString());
+        }
+        #endregion
+    }
+}
---
+++ Spicy/Spicy/DAL/Entities/Published.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Spicy.DAL.Entities
+{
+    class Published
+    {
+        #region Properties
+        public uint? Id_u { get; set; }
+        public uint? Id_p { get; set; }
+        #endregion
+
+        #region Constructors
+        public Published(MySqlDataReader reader)
+        {
+            Id_u = uint.Parse(reader["id_u"].ToString());
+            Id_p = uint.Parse(reader["id_p"].ToString());
+        }
+        #endregion
+
+    }
+}
