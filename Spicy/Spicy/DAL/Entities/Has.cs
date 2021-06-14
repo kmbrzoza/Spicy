ff891447ecc14ed3a785f82daa3e8e1cabc5fd72
Brzoza Krzysztof <kmbrzoza@gmail.com>
2021-06-14 16:33:53 +0200

    Added Has

---
+++ Spicy/Spicy/DAL/Entities/Has.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Spicy.DAL.Entities
+{
+    class Has
+    {
+        #region Properties
+        public uint? Id_has { get; set; }
+        public uint? Id_shop { get; set; }
+        public uint? Id_discount { get; set; }
+        #endregion
+
+        #region Constructors
+        public Has(MySqlDataReader reader)
+        {
+            Id_has = uint.Parse(reader["id_has"].ToString());
+            Id_shop = uint.Parse(reader["id_sshop"].ToString());
+            Id_discount = uint.Parse(reader["id_discount"].ToString());
+        }
+        #endregion
+    }
+}
