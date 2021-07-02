5a5f24469a6b5763277ba322897d5ce1881a8563
kami011 <kamila.g2067@gmail.com>
2021-07-02 17:01:28 +0200

    Some queries added

--- Spicy/Spicy/DAL/Repositories/CategoryRepo.cs
+++ Spicy/Spicy/DAL/Repositories/CategoryRepo.cs
@@ -8,5 +8,10 @@ namespace Spicy.DAL.Repositories
 {
     class CategoryRepo
     {
+        #region QUERIES
+        private const string GET_ALL_CATEGORIES = "SELECT * FROM category";
+        private const string GET_CATEGORIES_NAMES = "SELECT name FROM category";
+        private const string ADD_CATEGORY = "INSERT INTO `category`(`name`) VALUES ";//nwm czy to sie przyda ale jes
+        #endregion
     }
 }
--- Spicy/Spicy/DAL/Repositories/DiscountRepo.cs
+++ Spicy/Spicy/DAL/Repositories/DiscountRepo.cs
@@ -8,5 +8,21 @@ namespace Spicy.DAL.Repositories
 {
     class DiscountRepo
     {
+        #region QUERIES
+        private const string GET_ALL_DISCOUNTS = @"SELECT d.name, d.description, d.discount_code, d.start_date, d.end_date, d.curr_price, d.prev_price, u.name, c.name
+                                                    FROM discount d
+                                                    INNER JOIN publishedby p ON p.id_discount=d.id_discount
+                                                    INNER JOIN user u ON p.id_user=u.id_user
+                                                    INNER JOIN isin i ON i.id_discount=d.id_discount
+                                                    INNER JOIN category c ON c.id_category=i.id_category;";
+        private const string GET_DISCOUNTS_BY_CATEGORY = @"SELECT d.name, d.description, d.discount_code, d.start_date, d.end_date, d.curr_price, d.prev_price, u.name, c.name
+                                                    FROM discount d
+                                                    INNER JOIN publishedby p ON p.id_discount= d.id_discount
+                                                    INNER JOIN user u ON p.id_user= u.id_user
+                                                    INNER JOIN isin i ON i.id_discount= d.id_discount
+                                                    INNER JOIN category c ON c.id_category= i.id_category
+                                                    WHERE c.name=";
+        private const string ADD_DISCOUNT = @"INSERT INTO `discount`(`name`, `description`, `discount_code`, `start_date`, `end_date`, `curr_price`, `prev_price`) VALUES "; //nwm jeszcze od której strony sie za to zabrać póki co jest jak jest ^^
+        #endregion
     }
 }
