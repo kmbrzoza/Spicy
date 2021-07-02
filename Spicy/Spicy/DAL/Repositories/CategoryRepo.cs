f67c3a20ae5491584d64c81bd9be5b1d8b0c0fe6
kami011 <kamila.g2067@gmail.com>
2021-07-02 19:47:35 +0200

    More changes

--- Spicy/Spicy/DAL/Repositories/CategoryRepo.cs
+++ Spicy/Spicy/DAL/Repositories/CategoryRepo.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Spicy.DAL.Repositories
 {
+    using Entities;
     class CategoryRepo
     {
         #region QUERIES
         private const string GET_ALL_CATEGORIES = "SELECT * FROM category";
         private const string GET_CATEGORIES_NAMES = "SELECT name FROM category";
         private const string ADD_CATEGORY = "INSERT INTO `category`(`name`) VALUES ";//nwm czy to sie przyda ale jes
+        #endregion
+
+        #region METHODS
+        public static List<Category> GetCategories()
+        {
+            List <Category> categories= new List<Category>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(GET_CATEGORIES_NAMES, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    categories.Add(new Category(reader));
+                connection.Close();
+            }
+            return categories;
+        }
+
+
         #endregion
     }
 }
--- Spicy/Spicy/DAL/Repositories/ShopRepo.cs
+++ Spicy/Spicy/DAL/Repositories/ShopRepo.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
 
 namespace Spicy.DAL.Repositories
 {
+    using Entities;
     class ShopRepo
     {
         #region QUERIES
         private const string GET_SHOPS = "SELECT * FROM shop";
         private const string GET_SHOP_NAMES = "SELECT name FROM shop";
         private const string ADD_SHOP = "INSERT INTO `shop`(`name`) VALUES ";
+        #endregion
+
+        #region METHODS
+        public static bool AddShop(Shop shop)
+        {
+            bool status = false;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{ADD_SHOP} {shop.ToInsert()}", connection);
+                connection.Open();
+                var id = command.ExecuteNonQuery();
+                status = true;
+                shop.Id = (uint)command.LastInsertedId;
+                connection.Close();
+            }
+            return status;
+        }
+
         #endregion
     }
 }
