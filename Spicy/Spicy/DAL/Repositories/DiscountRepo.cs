bb025063a332284a192ae91babf9be37e7e4f897
kami011 <kamila.g2067@gmail.com>
2021-07-14 12:58:50 +0200

    Adding Discount with null prices

--- Spicy/Spicy/DAL/Repositories/DiscountRepo.cs
+++ Spicy/Spicy/DAL/Repositories/DiscountRepo.cs
@@ -14,6 +14,8 @@ namespace Spicy.DAL.Repositories
         #region QUERIES
         private const string GET_ALL_DISCOUNTS = @"SELECT * from discount";
         private const string ADD_DISCOUNT = @"INSERT INTO `discount`(`name`, `description`, `curr_price`, `prev_price`, `discount_code`, `start_date`, `end_date`, `link`, `image`, `id_category`, `id_user`, `id_shop`) VALUES ";
+        private const string ADD_DISCOUNT_PRICESNULL = @"INSERT INTO `discount`(`name`, `description`, `discount_code`, `start_date`, `end_date`, `link`, `image`, `id_category`, `id_user`, `id_shop`) VALUES ";
+
         #endregion
 
         #region METHODS
@@ -35,9 +37,17 @@ namespace Spicy.DAL.Repositories
         public static bool AddDiscount(Discount discount)
         {
             bool status = false;
+            MySqlCommand command = null;
             using (var connection = DBConnection.Instance.Connection)
             {
-                MySqlCommand command = new MySqlCommand($"{ADD_DISCOUNT} {discount.ToInsert()}", connection);
+                if (discount.PreviousPrice.HasValue == false & discount.CurrentPrice.HasValue == false)
+                {
+                    command = new MySqlCommand($"{ADD_DISCOUNT_PRICESNULL} {discount.ToInsert()}", connection);
+                }
+                else
+                {
+                    command = new MySqlCommand($"{ADD_DISCOUNT} {discount.ToInsert()}", connection);
+                }
                 connection.Open();
                 command.ExecuteNonQuery();
                 status = true;
@@ -50,9 +60,17 @@ namespace Spicy.DAL.Repositories
         public static bool UpdateDiscount(Discount discount, uint Id)
         {
             bool status = false;
+            MySqlCommand command = null;
             using (var connection = DBConnection.Instance.Connection)
             {
-                MySqlCommand command = new MySqlCommand($"UPDATE discount SET name = '{discount.Name}', description = '{discount.Description}, curr_price = '{discount.CurrentPrice}, prev_price = '{discount.PreviousPrice}, link = '{discount.Link}', discount_code = '{discount.Code}', start_date = '{discount.Start_Date}, end_date = '{discount.End_Date}', image = '{discount.Image}, id_category = '{discount.Id_category}', id_shop = '{discount.Id_shop}' WHERE id_discount = '{Id}'", connection);
+                if (!discount.PreviousPrice.HasValue && !discount.CurrentPrice.HasValue)
+                {
+                    command = new MySqlCommand($"UPDATE discount SET name = '{discount.Name}', description = '{discount.Description}, link = '{discount.Link}', discount_code = '{discount.Code}', start_date = '{discount.Start_Date}, end_date = '{discount.End_Date}', image = '{discount.Image}, id_category = '{discount.Id_category}', id_shop = '{discount.Id_shop}' WHERE id_discount = '{Id}'", connection);
+                }
+                else
+                {
+                    command = new MySqlCommand($"UPDATE discount SET name = '{discount.Name}', description = '{discount.Description}, curr_price = '{discount.CurrentPrice}, prev_price = '{discount.PreviousPrice}, link = '{discount.Link}', discount_code = '{discount.Code}', start_date = '{discount.Start_Date}, end_date = '{discount.End_Date}', image = '{discount.Image}, id_category = '{discount.Id_category}', id_shop = '{discount.Id_shop}' WHERE id_discount = '{Id}'", connection);
+                }
                 connection.Open();
                 var n = command.ExecuteNonQuery();
                 if (n == 1) status = true;
